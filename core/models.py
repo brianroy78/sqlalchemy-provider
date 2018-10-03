04a6ae652fe73356c66bd0a6776b04365fbf18bd
Brian Roy <brianroy78@hotmail.com>
2018-10-02 22:19:01 -0400

    create table method created

---
+++ core/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import (
+    Column,
+    Index,
+    Integer,
+    Text,
+)
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+# from test.models.meta import Base
+
+
+class MyModel(Base):
+    __tablename__ = 'models'
+    id = Column(Integer, primary_key=True)
+    name = Column(Text)
+    value = Column(Integer)
+
+
+Index('my_index', MyModel.name, unique=True, mysql_length=255)
--- provider.py
+++ provider.py
@@ -1,12 +1,37 @@
+import sqlite3
+from importlib import import_module
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+
+
 class DBSessionProvider:
+    url = None
+    engine = None
     session_maker = None
-    connection_string = 'sqlite:///scraping.db'
+    settings = None
+    models_package = None
+
+    @classmethod
+    def get_session(cls) -> Session:
+        return cls.session_maker()
 
     @staticmethod
-    def get_session() -> Session:
-        if DBSessionProvider.session_maker is None:
-            engine = create_engine(DBSessionProvider.connection_string)
-            sqlite3.connect(DBSessionProvider.connection_string.split('///')[-1])
-            Base.metadata.create_all(engine, checkfirst=True)
-            DBSessionProvider.session_maker = sessionmaker(bind=engine, autoflush=False)
-        return DBSessionProvider.session_maker()
+    def __get_module():
+        models_package = DBSessionProvider.settings['sqlalchemy']['models_package']
+        return import_module('%s.%s' % (models_package, 'models'))
+
+    @classmethod
+    def read(cls, settings):
+        cls.url = settings['sqlalchemy']['url']
+        cls.engine = create_engine(cls.url)
+        cls.session_maker = sessionmaker(bind=cls.engine, autoflush=False)
+        if 'squile' in cls.url:
+            sqlite3.connect(cls.url.split('///')[-1])
+        cls.models_package = settings['sqlalchemy']['models_package']
+
+    @classmethod
+    def create_tables(cls):
+        m = import_module('%s.%s' % (cls.models_package, 'models'))
+        base = getattr(m, 'Base')
+        base.metadata.create_all(cls.engine)
