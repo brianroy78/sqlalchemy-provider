98d366c2053b610d17952f680d9ccd41592a8ff0
Brian Roy Sagredo Lijeron <brianroy78@hotmail.com>
2022-06-10 09:16:27 -0400

    adding relation, foreignkey and db creation code snippets

--- example.py
+++ example.py
@@ -1,7 +1,10 @@
 from configparser import ConfigParser
 
-from models import set_settings
+from models import set_settings, Base, Data
 
 config = ConfigParser()
 config.read('settings.ini')
 set_settings(config['DEFAULT']['sqlalchemy.url'])
+
+# this will create the database
+Base.metadata.create_all(Data.ENGINE)
--- models/example_model.py
+++ models/example_model.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, relation
 
 from models import Base
 
@@ -8,6 +9,22 @@ class Example(Base):
     id = Column(Integer, primary_key=True)
     column1 = Column(String(80), unique=True, nullable=False)
     column2 = Column(String(120), unique=True, nullable=False)
+
+    # this is ORM (very different from the actual table cols, It uses class name instead of table name)
+    children = relationship("Child")
+
+    def __repr__(self):
+        return '<Example %r>' % self.column1
+
+
+class Child(Base):
+    __tablename__ = 'child'
+    id = Column(Integer, primary_key=True)
+    column1 = Column(String(80), unique=True, nullable=False)
+    column2 = Column(String(120), unique=True, nullable=False)
+    parent_id = Column(Integer, ForeignKey("parent.id"))
+
+    parent = relation('Example')
 
     def __repr__(self):
         return '<Example %r>' % self.column1
