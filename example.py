8138b6e5c2d9c61066ba2e55f37d8ec387e78a70
Brian Roy Sagredo Lijeron <brianroy78@hotmail.com>
2022-06-11 09:51:52 -0400

    how to create all the tables from model

--- example.py
+++ example.py
@@ -1,10 +1,12 @@
 from configparser import ConfigParser
 
 from models import set_settings, Base, Data
+from models.example_model import *  # it's better to import all
 
 config = ConfigParser()
 config.read('settings.ini')
 set_settings(config['DEFAULT']['sqlalchemy.url'])
 
 # this will create the database
+
 Base.metadata.create_all(Data.ENGINE)
